3c9b255c1f8d559c752b86933965a7ac7eb02279
Duc Le Dang <51703059@student.tdtu.edu.vn>
2020-02-01 05:35:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 ==0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif num % 3 == 0 && num % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   else
-    puts nil
+    nil
   end
 end
 puts fizzbuzz(5)
